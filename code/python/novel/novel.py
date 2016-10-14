1622b80c6fc12ba418afaa4b1be951678e092d37
hadesshark <hades_shark@hotmail.com>
2016-10-14 14:39:02 +0800

    想到 windows 編碼問題，決定先把 code 放到 dropbox。

---
+++ code/python/novel/novel.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env/python
+# -*- coding: utf-8 -*-
+
+# chinese can't use.
+# now use windows -> mac test try if can use.
+
+import requests
+
+headers = {
+	'User-Agent':
+	'Mozilla/5.0 (Windows NT 6.1) Chrome/44.0.2403.157 Safari/537.36'}
+
+url = "http://ck101.com/thread-2462938-1-1.html"
+request_get = requests.get(url, headers=headers)
+print(request_get.text)
