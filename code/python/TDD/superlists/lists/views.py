380ccaecd1c8a6372137dc000875dcaf84242d2c
hadesshark <hades_shark@hotmail.com>
2016-11-08 18:08:02 +0800

    Error 的部分一樣，但因為我沒注意到 view 的第六行 HttpResponse 沒修改到。

--- code/python/TDD/superlists/lists/views.py
+++ code/python/TDD/superlists/lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # 在這複創造你的 view
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return HttpResponse(request, 'home.html')
