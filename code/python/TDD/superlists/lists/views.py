887cf8276bf94379a1f324668d31f6a0687b8ae2
hadesshark <hades_shark@hotmail.com>
2016-11-08 17:41:47 +0800

    ok 錯誤沒有了。

--- code/python/TDD/superlists/lists/views.py
+++ code/python/TDD/superlists/lists/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # 在這複創造你的 view
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title>')
+    return HttpResponse('<html><title>To-Do lists</title></html>')
