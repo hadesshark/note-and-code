ecd055373e370af56183e532e849d5436e829fa6
hadesshark <hades_shark@hotmail.com>
2016-11-09 21:38:23 +0800

    這一步書上說會錯，但我執行後確是正確的。

--- code/python/TDD/superlists/lists/views.py
+++ code/python/TDD/superlists/lists/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # 在這複創造你的 view
 def home_page(request):
     return render(request, 'home.html', {
-        'new_item_text': request.POST['item_text'],
+        'new_item_text': request.POST.get('item_text', ''),
     })
