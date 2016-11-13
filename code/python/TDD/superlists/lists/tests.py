72cd2eefc5c56f9e80a7219bd77d58a36373f3a8
hadesshark <hades_shark@hotmail.com>
2016-11-13 09:34:26 +0800

    錯誤已調整，不用 debug。

--- code/python/TDD/superlists/lists/tests.py
+++ code/python/TDD/superlists/lists/tests.py
@@ -25,8 +25,8 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(Item.objects.count(), 1)
-        mew_item = Item.objects.first()
-        self.assertEqual(new_item_text, 'A new list item')
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
         self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
