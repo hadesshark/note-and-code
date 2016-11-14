82b61aa509d66e815174df05b3c91bceec9d55c8
hadesshark <hades_shark@hotmail.com>
2016-11-15 07:58:11 +0800

    url 方法已新增，雖然沒有多大的進步。

--- code/python/TDD/superlists/functional_tests/tests.py
+++ code/python/TDD/superlists/functional_tests/tests.py
@@ -79,6 +79,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # Francis 取得他自己的獨一無二 URL
+        time.sleep(2)
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
