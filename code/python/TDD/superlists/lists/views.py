76f6ba1cbdb54fbbd3b6ec0cc611ba6be0ac5f55
hadesshark <hades_shark@hotmail.com>
2016-11-15 18:30:58 +0800

    預期錯誤。

--- code/python/TDD/superlists/lists/views.py
+++ code/python/TDD/superlists/lists/views.py
@@ -14,4 +14,5 @@ def view_list(request):
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-    return redirect('/lists/the-only-list-in-the-world/')    
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
