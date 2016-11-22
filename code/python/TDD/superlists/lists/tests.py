1212b20e11d94aa836dafc21677fc2013082e9f3
hadesshark <hades_shark@hotmail.com>
2016-11-22 23:09:55 +0800

    這個錯誤有點怪異。

--- code/python/TDD/superlists/lists/tests.py
+++ code/python/TDD/superlists/lists/tests.py
@@ -1,3 +1,4 @@
+#! /usr/local/bin/python3.5
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
@@ -64,8 +65,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+    	list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
