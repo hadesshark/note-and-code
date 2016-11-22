93e4a5dc60b377186ffb393e2d8113e9255809c4
hadesshark <hades_shark@hotmail.com>
2016-11-22 23:22:03 +0800

    原來是 tab 問題。 這此的錯誤在 29 和 89 行。

--- code/python/TDD/superlists/lists/tests.py
+++ code/python/TDD/superlists/lists/tests.py
@@ -65,7 +65,7 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-    	list_ = List.objects.create()
+        list_ = List.objects.create()
         Item.objects.create(text='itemey 1', list=list_)
         Item.objects.create(text='itemey 2', list=list_)
 
