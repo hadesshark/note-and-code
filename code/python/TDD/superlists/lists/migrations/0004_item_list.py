347ce3f3078854461767548b13a30ae57fc67edf
hadesshark <hades_shark@hotmail.com>
2016-11-22 22:55:12 +0800

    不了解的動作，其中有刪除遷移。

--- code/python/TDD/superlists/lists/migrations/0004_item_list.py
+++ code/python/TDD/superlists/lists/migrations/0004_item_list.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='item',
             name='list',
-            field=models.TextField(default=''),
+            field=models.ForeignKey(default=None, to='lists.List'),
         ),
     ]
