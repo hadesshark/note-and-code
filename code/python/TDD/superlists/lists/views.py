317727495d00ffbfa70545906930b0d3cb32c4a5
hadesshark <hades_shark@hotmail.com>
2016-11-23 18:25:29 +0800

    又多了一個錯誤訊息，也是預期錯誤。

--- code/python/TDD/superlists/lists/views.py
+++ code/python/TDD/superlists/lists/views.py
@@ -7,6 +7,7 @@ def home_page(request):
     return render(request, 'home.html')
 
 def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 
