bbd950bb92b4619da006e24f3a87e04247178326
hadesshark <hades_shark@hotmail.com>
2016-11-24 18:07:37 +0800

    跳過兩次單元測試，但沒關係，都是預期錯誤。
    # 一個是只增加 add_item(request)
    # 一個是多增加 add_item(request, list_id)

--- code/python/TDD/superlists/lists/views.py
+++ code/python/TDD/superlists/lists/views.py
@@ -15,3 +15,7 @@ def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/%d/' % (list_.id,))
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return redirect('/lists/%d/' % (list_.id,))
